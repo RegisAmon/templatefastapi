ae9e29757e55e7b1d9200aa7f25b83ff7149c1cf
RegisAmon <82566945+RegisAmon@users.noreply.github.com>
2022-08-25 11:49:31 +0200

    Update main.py

--- application/server/main.py
+++ application/server/main.py
@@ -144,51 +144,6 @@ def video_mamonreader(cv2,filename):
 
 
 
-
-def mamon_videoFightModel2(tf,wight='mamonbest947oscombo.hdfs'):
-    layers = tf.contrib.keras.layers
-    models = tf.contrib.keras.models
-    losses = tf.contrib.keras.losses
-    optimizers = tf.contrib.keras.optimizers
-    metrics = tf.contrib.keras.metrics
-    num_classes = 2
-    cnn = models.Sequential()
-    #cnn.add(base_model)
-
-    input_shapes=(160,160,3)
-    np.random.seed(1234)
-    vg19 = tf.keras.applications.vgg19.VGG19
-    base_model = vg19(include_top=False,weights='imagenet',input_shape=(160, 160,3))
-    # Freeze the layers except the last 4 layers
-    #for layer in base_model.layers:
-    #    layer.trainable = False
-
-    cnn = models.Sequential()
-    cnn.add(base_model)
-    cnn.add(layers.Flatten())
-    model = models.Sequential()
-
-    model.add(layers.TimeDistributed(cnn,  input_shape=(30, 160, 160, 3)))
-    model.add(layers.LSTM(30 , return_sequences= True))
-
-    model.add(layers.TimeDistributed(layers.Dense(90)))
-    model.add(layers.Dropout(0.1))
-
-    model.add(layers.GlobalAveragePooling1D())
-
-    model.add(layers.Dense(512, activation='relu'))
-    model.add(layers.Dropout(0.3))
-
-    model.add(layers.Dense(num_classes, activation="sigmoid"))
-
-    adam = optimizers.Adam(lr=0.0005, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-    model.load_weights(wight)
-    rms = optimizers.RMSprop()
-
-    model.compile(loss='binary_crossentropy', optimizer=adam, metrics=["accuracy"])
-
-    return model
-
 def pred_fight(video,acuracy=0.85):
   ysvid2 = video_mamonreader(cv2,video)
 
